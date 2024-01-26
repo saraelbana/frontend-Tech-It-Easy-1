0cb553f88f1efd00dfaf08c200079d380fb8944c
saraelbana <matika20@gmail.com>
2024-01-26 11:54:41 +0100

    starting to create a generc omponent for the verkoopoverzicht article in the first section

--- src/App.jsx
+++ src/App.jsx
@@ -2,9 +2,16 @@ import './App.css';
 
 function App() {
   return (
-    <h1>Tech IT easy dashboard</h1>
+    <h1>Tech IT easy dashboard</h1>,
+    <section>
+      <h2>
+        VerkoopOverzicht
+      </h2>
+      <div id="sellingOverviewArticle"></div>
+      <script type="module" src="/src/components/sellingoverviewarticle.jsx"></script>
 
-  )
+    </section>
+  );
 }
 
 export default App
---
+++ src/components/sellingoverviewarticle.jsx
@@ -0,0 +1,13 @@
+
+function article(cmdData) {
+    return (
+        <article>
+            <h3>
+                cmdData.title
+            </h3>
+            <p>
+                cmdData.price
+            </p>
+        </article>
+    );
+}
